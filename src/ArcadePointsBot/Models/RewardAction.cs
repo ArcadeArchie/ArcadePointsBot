30b3d5db14a7f9c994a6c8385b270ae160b1e550
ArcadeArchie <29282748+ArcadeArchie@users.noreply.github.com>
2023-11-14 23:32:33 +0100

    Apply enum translations to UI

--- src/ArcadePointsBot/Models/RewardAction.cs
+++ src/ArcadePointsBot/Models/RewardAction.cs
@@ -1,44 +1,48 @@
-﻿using Avalonia.Input;
-using ArcadePointsBot.Data.Abstractions;
-using ArcadePointsBot.Util;
-using ArcadePointsBot.ViewModels;
-using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
+﻿using ArcadePointsBot.Data.Abstractions;
 using System;
-using System.Collections;
+using System.ComponentModel;
 
 namespace ArcadePointsBot.Models;
-public abstract class RewardAction : IEntity<string> 
+public abstract class RewardAction : IEntity<string>
 {
     public string Id { get; set; } = null!;
     public int Index { get; set; }
     public TwitchReward Reward { get; set; } = null!;
     public int? Duration { get; set; }
 }
-public abstract class RewardAction<TType, TKey> : RewardAction 
-    where TType : struct, Enum  
-    where TKey : struct, Enum 
-{    
+public abstract class RewardAction<TType, TKey> : RewardAction
+    where TType : struct, Enum
+    where TKey : struct, Enum
+{
     public TType ActionType { get; set; }
     public TKey ActionKey { get; set; }
 }
 
 public enum ActionType
 {
+    [Description("ActionType_Keyboard")]
     Keyboard,
+    [Description("ActionType_Mouse")]
     Mouse
 }
 
 public enum KeyboardActionType
 {
+    [Description("KeyboardActionType_Tap")]
     Tap,
+    [Description("KeyboardActionType_Press")]
     Press,
+    [Description("KeyboardActionType_Release")]
     Release
 }
 public enum MouseActionType
 {
+    [Description("MouseActionType_Click")]
     Click,
+    [Description("MouseActionType_DoubleClick")]
     DoubleClick,
+    [Description("KeyboardActionType_Press")]
     Press,
+    [Description("KeyboardActionType_Release")]
     Release
 }
--- src/ArcadePointsBot/ViewModels/RewardActionViewModel.cs
+++ src/ArcadePointsBot/ViewModels/RewardActionViewModel.cs
@@ -6,59 +6,52 @@ using Avalonia.Input;
 using ReactiveUI.Fody.Helpers;
 using System;
 
-namespace ArcadePointsBot.ViewModels
+namespace ArcadePointsBot.ViewModels;
+
+public class RewardActionViewModel : ReactiveObject
 {
-    public class RewardActionViewModel : ReactiveObject
+    public string? Id { get; init; }
+    [Reactive] public int? Duration { get; set; }
+    [Reactive] public ActionType? ActionType { get; set; }
+    [Reactive] public Enum? ActionKeyType { get; set; }
+    [Reactive] public Enum? ActionKey { get; set; }
+    public int Index { get; set; }
+
+
+    public RewardActionViewModel()
     {
 
-        public IEnumerable ActionValues { get; } = EnumUtils.GetValues<ActionType>();
-        public IEnumerable MouseActionValues { get; } = EnumUtils.GetValues<MouseActionType>();
-        public IEnumerable KeyboardActionValues { get; } = EnumUtils.GetValues<KeyboardActionType>();
-        public IEnumerable MouseValues { get; } = EnumUtils.GetValues<MouseButton>();
-        public IEnumerable KeyboardValues { get; } = EnumUtils.GetValues<Key>();
-        public string? Id { get; init; }
-        [Reactive] public int? Duration { get; set; }
-        [Reactive] public ActionType? ActionType { get; set; }
-        [Reactive] public Enum? ActionKeyType { get; set; }
-        [Reactive] public Enum? ActionKey { get; set; }
-        public int Index { get; set; }
+    }
 
+    public RewardActionViewModel(int index)
+    {
+        Index = index;
+    }
 
-        public RewardActionViewModel()
+    internal static RewardActionViewModel FromAction(RewardAction action)
+    {
+        switch (action)
         {
-
-        }
-
-        public RewardActionViewModel(int index)
-        {
-            Index = index;
-        }
-
-        internal static RewardActionViewModel FromAction(RewardAction action)
-        {
-            switch (action)
-            {
-                case MouseRewardAction actual:
-                    return new RewardActionViewModel(action.Index)
-                    {
-                        Id = action.Id,
-                        Duration = action.Duration,
-                        ActionType = Models.ActionType.Mouse,
-                        ActionKeyType = actual.ActionType,
-                        ActionKey = actual.ActionKey,
-                    };
-                case KeyboardRewardAction actual:
-                    return new RewardActionViewModel(action.Index)
-                    {
-                        Id = action.Id,
-                        Duration = action.Duration,
-                        ActionType = Models.ActionType.Keyboard,
-                        ActionKeyType = actual.ActionType,
-                        ActionKey = actual.ActionKey,
-                    };
-                default:
-                    throw new InvalidOperationException("Action type not supported");
-            }
+            case MouseRewardAction actual:
+                return new RewardActionViewModel(action.Index)
+                {
+                    Id = action.Id,
+                    Duration = action.Duration,
+                    ActionType = Models.ActionType.Mouse,
+                    ActionKeyType = actual.ActionType,
+                    ActionKey = actual.ActionKey,
+                };
+            case KeyboardRewardAction actual:
+                return new RewardActionViewModel(action.Index)
+                {
+                    Id = action.Id,
+                    Duration = action.Duration,
+                    ActionType = Models.ActionType.Keyboard,
+                    ActionKeyType = actual.ActionType,
+                    ActionKey = actual.ActionKey,
+                };
+            default:
+                throw new InvalidOperationException("Action type not supported");
         }
     }
 }
